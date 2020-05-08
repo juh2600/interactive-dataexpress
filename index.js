9e6feac6408730b8a4b3c3ed635825613ce11905
Ryan <ryanunroe@gmail.com>
2020-05-08 13:51:25 -0600

    Created html for public pug files

--- index.js
+++ index.js
@@ -11,7 +11,23 @@ app.use(express.static(path.join(__dirname+"/public")));
 
 
 app.get("/", (req, res) => {
-    res.render("landing", {});
-})
+    res.render("landing");
+});
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
+
+
+
+app.get("/account/edit", (req, res) => {
+    res.render("accountEdit");
+});
+app.get("/dashboard", (req, res) => {
+    res.render("dashboard");
+});
+
 
 app.listen(3000);
