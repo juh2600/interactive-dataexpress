0f3d3e08ca4ebd0dc6f1d495e9d7544744d34542
Joseph Reed <josephreed2600@gmail.com>
2020-05-14 18:35:33 -0600

    start refactoring index

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
+const logger = require('./logger').get('main')
 const express = require("express");
 const expressSession = require('express-session');
 const pug = require("pug");
 const path = require("path");
 
 const app = express();
-/*Making an interceptor*/
-const express = require('express');
 
-const app = express()
+/*Making an interceptor*/
 const intercept = (req, res, next) => {
     console.log('Intercepted on ', req.path);
     next();
@@ -17,6 +16,7 @@ app.get('/', (req, res) => {
     console.log('You must be logged in.');
     res.send('You must be logged in.');
 });
+
 /*Body parser and a little interceptor usage*/
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({extended: false});
@@ -30,11 +30,12 @@ const checkAuth = (req, res, next) => {
 };
 
 app.use(expressSession({
-    secret: '*enter secret here*',
+    secret: process.env.EXPRESS_SESSION_SECRET,
     saveUninitialized: true,
     resave: true
 }));
 
+/*
 app.post('/login', urlencodedParser, (req, res) => {
     console.log(req.body.username);
     if(req.body.username == account.username && req.body.password == account.password){
@@ -47,13 +48,13 @@ app.post('/login', urlencodedParser, (req, res) => {
         res.redirect('/login');
     }
 });
-
+*/
 /*Public pages*/
 app.set("view engine", "pug");
 app.set("views", __dirname+"/views");
 app.use(express.static(path.join(__dirname+"/public")));
 
-
+/*
 app.get("/", (req, res) => {
     res.render("landing");
 });
@@ -63,8 +64,9 @@ app.get("/login", (req, res) => {
 app.get("/signup", (req, res) => {
     res.render("signup");
 });
-
+*/
 /*Private Pages*/
+/*
 app.get("/account/edit", checkAuth, (req, res) => {
     res.render("accountEdit");
     res.send("You must be signed in!")
@@ -73,6 +75,21 @@ app.get("/dashboard", checkAuth, (req, res) => {
     res.render("dashboard");
     res.send('You must be singed in!')
 });
+*/
 
+logger.info('Configuring routes...');
+let routeFiles = ['frontend', 'accounts'];
+const routeManager = require('./routes/manager');
+routeFiles.forEach((file) => {
+        logger.info(`Adding ${file} routes...`);
+        let component = require(`./routes/${file}`);
+        if(component.configure) component.configure({
+                player,
+                media_dir: process.env.MEDIA_DIR
+        });
+        routeManager.apply(app, component);
+        logger.info(`Added ${file} routes.`);
+});
+logger.info('Configured routes.');
 
 app.listen(3000);
