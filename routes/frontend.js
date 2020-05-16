abaefe554f59f7ec202ce5c84577943a084dc40b
Joseph Reed <josephreed2600@gmail.com>
2020-05-16 17:57:12 -0600

    add notes

--- routes/frontend.js
+++ routes/frontend.js
@@ -27,10 +27,12 @@ const signUp = (req, res) => {
 	res.render('signup', { session: req.session });
 };
 
+// TODO implement
 const logout = (req, res) => {
 	res.redirect('/', { session: req.session });
 };
-//POST
+
+// FIXME redirect back to login page on failure, with message
 const loginPost = (req, res) => {
 	requirePresenceOfParameter(req.body.username, "username", res);
 	requirePresenceOfParameter(req.body.password, "password", res);
@@ -62,6 +64,7 @@ const dashboard = (req, res) => {
 };
 
 
+// TODO implement
 const editAccount = (req, res) => {
 	res.render('accountEdit', {
 		session: req.session
@@ -122,76 +125,3 @@ const configure = options => {
 }
 
 module.exports = { logger, routes, configure };
-
-/*Joe's Code Examples with me adding to them*/
-/*
-accounts.create = (req, res) => {
-	let account = new Account({
-		username: String,
-		password: String,
-		email: String,
-		dob: Date,
-		questions: [
-			{
-				id: Number,
-				answer: String
-			},
-			{
-				id: Number,
-				answer: String
-			},
-			{
-				id: Number,
-				answer: String
-			}
-		]
-	});
-	res.redirect('/');
-}
-
-accounts.get(username);
-
-accounts.edit(username, {
-	//object containing any of the things above
-	// account.username = req.body.username,
-	// account
-});
-
-// accounts.remove(username);
-
-
-// accounts.authenticate(username, password);
-
-
-// const clearCart = (req, res) => {
-//     let customer_id = req.body.customer_id;
-//     if(!requirePresenceOfParameter(customer_id, 'customer_id', res)) return;
-
-//     try {
-//             api.clearCart(customer_id);
-//     } catch(err) {
-//             if(/Customer .* does not exist/i.test(err)) {
-// // respond is a function i wrote elsewhere; use res.status and res.send
-//                     respond(404, err, res);
-//             } else {
-//                     respond(500, err, res);
-//             }
-//             return;
-//     }
-//     respond(204, `Cart ${customer_id} is empty`, res);
-// };
-
-
-// const respond = (code, why, res) => {
-// if(!res) throw `Missing response object`;
-// res.statusMessage = why;
-// res.status(code).end();
-// };
-
-// const requirePresenceOfParameter = (param, name, res) => {
-// if(!param) {
-//     respond(400, `Missing parameter ${name}`, res);
-//     return false;
-// } else return true;
-// };
- */
